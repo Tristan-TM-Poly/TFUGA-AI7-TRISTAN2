2a1b063b043341baef11c1eb67b3b6070f25ba60
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-26 04:55:14 -0400

    Export AUTO2 proof telemetry improver APIs

--- omega_auto2_kernel/omega_auto2/__init__.py
+++ omega_auto2_kernel/omega_auto2/__init__.py
@@ -1,4 +1,4 @@
-"""Ω-AUTO²-Kernel v0.2.
+"""Ω-AUTO²-Kernel v0.3.
 
 Prototype OAK-safe d'automatisation de l'automatisation pour Tristan.
 """
@@ -9,6 +9,9 @@ from .workflow_synth import forge_workflow_from_task
 from .oak_gate import evaluate_workflow
 from .capabilities import CapacityVector, CapabilityAssessment, assess_capability, infer_capacity_vector
 from .sandbox import DryRunReport, dry_run_workflow
+from .telemetry import TelemetrySnapshot
+from .proof import ProofOfWorkflow, prove_workflow
+from .improver import improve_draft
 
 __all__ = [
     "FrictionTensor",
@@ -23,4 +26,8 @@ __all__ = [
     "infer_capacity_vector",
     "DryRunReport",
     "dry_run_workflow",
+    "TelemetrySnapshot",
+    "ProofOfWorkflow",
+    "prove_workflow",
+    "improve_draft",
 ]
