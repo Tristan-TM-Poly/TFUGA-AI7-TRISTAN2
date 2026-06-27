e035225b59d22ae5cc1ab29d46dadbbf7fe951e1
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-27 03:35:32 -0400

    Export DemoForge-T types

--- omega_game_t/omega_game/forge/__init__.py
+++ omega_game_t/omega_game/forge/__init__.py
@@ -1,9 +1,14 @@
 """Forge modules for Ω-GAME-T++ production pipelines."""
 
+from .demo_forge import DemoForge, DemoPlan, DemoScene, default_demo_forge
 from .issue_forge import IssueForge, IssueSet, IssueSpec, LabelPlan, MilestonePlan, default_issue_forge
 from .sprint_forge import SprintForge, SprintPlan, SprintTask, default_sprint_forge
 
 __all__ = [
+    "DemoForge",
+    "DemoPlan",
+    "DemoScene",
+    "default_demo_forge",
     "IssueForge",
     "IssueSet",
     "IssueSpec",
