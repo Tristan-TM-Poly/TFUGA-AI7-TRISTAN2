c047c553d647b41ec82bc560ea61fa59c596a08c
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-27 13:14:01 -0400

    Export LanguageCurriculum-T

--- omega_game_t/omega_game/engines/__init__.py
+++ omega_game_t/omega_game/engines/__init__.py
@@ -1,9 +1,21 @@
 """Omega GAME T engine split units."""
 
+from .language_curriculum import (
+    CurriculumProgress,
+    CurriculumQuest,
+    CurriculumTrack,
+    LanguageCurriculum,
+    default_language_curriculum,
+)
 from .language_gm_rubric import LanguageGMEvaluation, LanguageGMRubric, LanguageRubricScores
 from .polyglot_language import LanguageQuest, LanguageRun, PolyglotLanguageEngine
 
 __all__ = [
+    "CurriculumProgress",
+    "CurriculumQuest",
+    "CurriculumTrack",
+    "LanguageCurriculum",
+    "default_language_curriculum",
     "LanguageGMEvaluation",
     "LanguageGMRubric",
     "LanguageQuest",
