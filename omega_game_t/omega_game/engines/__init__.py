63270903f4033a44c34660a5967ebdff77776225
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-27 13:00:18 -0400

    Add engines package for PolyglotLanguageEngine-T

---
+++ omega_game_t/omega_game/engines/__init__.py
@@ -0,0 +1,5 @@
+"""Omega GAME T engine split units."""
+
+from .polyglot_language import LanguageQuest, LanguageRun, PolyglotLanguageEngine
+
+__all__ = ["LanguageQuest", "LanguageRun", "PolyglotLanguageEngine"]
