2f1a7da3a382eb48b7b96265ff25923ff2d5d8b2
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-27 13:08:14 -0400

    Export LanguageGM Rubric-T

--- omega_game_t/omega_game/engines/__init__.py
+++ omega_game_t/omega_game/engines/__init__.py
@@ -1,5 +1,13 @@
 """Omega GAME T engine split units."""
 
+from .language_gm_rubric import LanguageGMEvaluation, LanguageGMRubric, LanguageRubricScores
 from .polyglot_language import LanguageQuest, LanguageRun, PolyglotLanguageEngine
 
-__all__ = ["LanguageQuest", "LanguageRun", "PolyglotLanguageEngine"]
+__all__ = [
+    "LanguageGMEvaluation",
+    "LanguageGMRubric",
+    "LanguageQuest",
+    "LanguageRubricScores",
+    "LanguageRun",
+    "PolyglotLanguageEngine",
+]
