8a89c01d75f24148304bc2a1db664b2d38a426c8
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-27 03:43:15 -0400

    Export RevenueForge-T and ProductBench-T types

--- omega_game_t/omega_game/forge/__init__.py
+++ omega_game_t/omega_game/forge/__init__.py
@@ -3,6 +3,16 @@
 from .demo_forge import DemoForge, DemoPlan, DemoScene, default_demo_forge
 from .issue_forge import IssueForge, IssueSet, IssueSpec, LabelPlan, MilestonePlan, default_issue_forge
 from .launch_forge import LandingPageDraft, LaunchDraft, LaunchForge, PitchDraft, default_launch_forge
+from .product_bench import ProductBench, ProductBenchMetrics, ProductBenchResult, default_product_bench
+from .revenue_forge import (
+    ChannelMap,
+    OfferSpec,
+    PricingHypothesis,
+    RevenueForge,
+    RevenuePlan,
+    RevenueSignal,
+    default_revenue_forge,
+)
 from .sprint_forge import SprintForge, SprintPlan, SprintTask, default_sprint_forge
 
 __all__ = [
@@ -21,6 +31,17 @@ __all__ = [
     "LaunchForge",
     "PitchDraft",
     "default_launch_forge",
+    "ProductBench",
+    "ProductBenchMetrics",
+    "ProductBenchResult",
+    "default_product_bench",
+    "ChannelMap",
+    "OfferSpec",
+    "PricingHypothesis",
+    "RevenueForge",
+    "RevenuePlan",
+    "RevenueSignal",
+    "default_revenue_forge",
     "SprintForge",
     "SprintPlan",
     "SprintTask",
