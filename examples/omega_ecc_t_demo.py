60a806b1dbc108ca8a52512201870ae3eb302735
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-27 13:18:26 -0400

    Show min-sum decoder in Ω-ECC-T demo

--- examples/omega_ecc_t_demo.py
+++ examples/omega_ecc_t_demo.py
@@ -5,7 +5,7 @@ Usage:
 """
 from __future__ import annotations
 
-from ecc_tristan import HyperParityGraph, SparseLDPC, bench_hamming74_bsc, decode, default_oakbench_matrix, encode
+from ecc_tristan import HyperParityGraph, SparseLDPC, bench_hamming74_bsc, decode, default_oakbench_matrix, encode, min_sum_decode
 from ecc_tristan.channels import burst_flip_channel
 from ecc_tristan.oak import gate_hamming74
 from ecc_tristan.soft_channels import bpsk_awgn_channel, sigma_from_ebn0_db
@@ -27,7 +27,8 @@ def main() -> None:
     print("hyper_parity_graph", graph.nearest_codeword([1, 0, 1]))
 
     ldpc = SparseLDPC.toy_6_3()
-    print("toy_ldpc", ldpc.bit_flip_decode([0, 0, 0, 1, 0, 0]))
+    print("toy_ldpc_hard", ldpc.bit_flip_decode([0, 0, 0, 1, 0, 0]))
+    print("toy_ldpc_min_sum", min_sum_decode(ldpc, [5.0, 5.0, 5.0, -1.0, 5.0, 5.0]))
 
     sigma = sigma_from_ebn0_db(rate=0.5, ebn0_db=3.0)
     soft = bpsk_awgn_channel([0, 1, 0, 1], sigma=sigma, seed=123)
