c2cf69273a24f20b5bd5e5094535d891cebc9a6d
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-27 13:14:11 -0400

    Export LanguageCurriculum-T at package root

--- omega_game_t/omega_game/__init__.py
+++ omega_game_t/omega_game/__init__.py
@@ -4,7 +4,19 @@ Small, testable subset extracted from the larger GAME branch.
 """
 
 from .core import Entity, Event, RuleKernel, WorldGraph, GameQualityScore
-from .engines import LanguageGMEvaluation, LanguageGMRubric, LanguageQuest, LanguageRubricScores, LanguageRun, PolyglotLanguageEngine
+from .engines import (
+    CurriculumProgress,
+    CurriculumQuest,
+    CurriculumTrack,
+    LanguageCurriculum,
+    LanguageGMEvaluation,
+    LanguageGMRubric,
+    LanguageQuest,
+    LanguageRubricScores,
+    LanguageRun,
+    PolyglotLanguageEngine,
+    default_language_curriculum,
+)
 from .oak import OAKGate, OAKReport
 
 __all__ = [
@@ -13,6 +25,11 @@ __all__ = [
     "RuleKernel",
     "WorldGraph",
     "GameQualityScore",
+    "CurriculumProgress",
+    "CurriculumQuest",
+    "CurriculumTrack",
+    "LanguageCurriculum",
+    "default_language_curriculum",
     "LanguageGMEvaluation",
     "LanguageGMRubric",
     "LanguageQuest",
