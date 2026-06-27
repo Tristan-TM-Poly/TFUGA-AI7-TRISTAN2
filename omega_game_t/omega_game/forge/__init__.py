7ff4804e56d07185c380a1acaf3389af6fd14caf
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-27 03:38:59 -0400

    Export LaunchForge-T types

--- omega_game_t/omega_game/forge/__init__.py
+++ omega_game_t/omega_game/forge/__init__.py
@@ -2,6 +2,7 @@
 
 from .demo_forge import DemoForge, DemoPlan, DemoScene, default_demo_forge
 from .issue_forge import IssueForge, IssueSet, IssueSpec, LabelPlan, MilestonePlan, default_issue_forge
+from .launch_forge import LandingPageDraft, LaunchDraft, LaunchForge, PitchDraft, default_launch_forge
 from .sprint_forge import SprintForge, SprintPlan, SprintTask, default_sprint_forge
 
 __all__ = [
@@ -15,6 +16,11 @@ __all__ = [
     "LabelPlan",
     "MilestonePlan",
     "default_issue_forge",
+    "LandingPageDraft",
+    "LaunchDraft",
+    "LaunchForge",
+    "PitchDraft",
+    "default_launch_forge",
     "SprintForge",
     "SprintPlan",
     "SprintTask",
