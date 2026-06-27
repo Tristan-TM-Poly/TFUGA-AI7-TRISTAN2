9ca056b9f67f7a2797e769543f3a29192a26f096
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-27 08:18:04 -0400

    Add GameMasterAcademy-T package exports

---
+++ omega_game_t/omega_game/masters/__init__.py
@@ -0,0 +1,21 @@
+"""GameMaster Academy primitives."""
+
+from .academy import (
+    AcademyEvaluation,
+    EvaluationRubric,
+    GameMasterAcademy,
+    GameMasterProfile,
+    SkillScore,
+    TrainingQuest,
+    default_gamemaster_academy,
+)
+
+__all__ = [
+    "AcademyEvaluation",
+    "EvaluationRubric",
+    "GameMasterAcademy",
+    "GameMasterProfile",
+    "SkillScore",
+    "TrainingQuest",
+    "default_gamemaster_academy",
+]
