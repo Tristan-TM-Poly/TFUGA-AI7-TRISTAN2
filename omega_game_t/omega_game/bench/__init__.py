a8a786c5c0fed88b00e86e0f186194b91e77680a
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-27 02:29:51 -0400

    Add OAKBench-GAME-T package exports

---
+++ omega_game_t/omega_game/bench/__init__.py
@@ -0,0 +1,5 @@
+"""OAKBench-GAME-T benchmarks for Ω-GAME-T++."""
+
+from .oakbench_game import OAKBenchMetrics, OAKBenchResult, OAKBenchRunner, default_engine_benchmarks
+
+__all__ = ["OAKBenchMetrics", "OAKBenchResult", "OAKBenchRunner", "default_engine_benchmarks"]
