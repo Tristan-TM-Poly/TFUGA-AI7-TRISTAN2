c08fb48642bd2559a1037c957fc6337ecb8d62bb
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-28 09:28:26 -0400

    Expose Ω-VTP-T v2 APIs

--- omega_vtp_t/__init__.py
+++ omega_vtp_t/__init__.py
@@ -8,6 +8,11 @@ Core safety rule:
 """
 
 from .adaptive_tensorprod import AdaptiveLiftFit, AdaptiveStep, adaptive_dynamic_lift_fit
+from .bases import ScalingDomain, chebyshev_lift, chebyshev_values, infer_scaling_domain, scale_to_unit_interval
+from .closure_residual import ClosureReport, closure_residual
+from .conditioning import OAKScore, ResidualMetrics, matrix_condition_number, oak_score, residual_metrics
+from .cvcd_selector import CVCDSelection, FeatureFertility, feature_fertility_scores, select_cvcd_features
+from .koopman_tensorprod import KoopmanTensorProdFit, fit_koopman_tensorprod, predict_lifted
 from .sparse_tensorprod import (
     SparseLiftResult,
     SparseSelectionReport,
@@ -26,23 +31,45 @@ from .tensor_prod_lift import (
     polynomial_eval_from_lift,
     tensor_prod_lift,
 )
+from .train_test_oak import TrainTestOAKReport, train_test_koopman_oak
 
 __all__ = [
     "AdaptiveLiftFit",
     "AdaptiveStep",
+    "CVCDSelection",
+    "ClosureReport",
+    "FeatureFertility",
+    "KoopmanTensorProdFit",
     "LiftResult",
     "LinearOperatorFit",
     "OAKReport",
+    "OAKScore",
+    "ResidualMetrics",
+    "ScalingDomain",
     "SparseLiftResult",
     "SparseSelectionReport",
+    "TrainTestOAKReport",
     "adaptive_dynamic_lift_fit",
+    "chebyshev_lift",
+    "chebyshev_values",
+    "closure_residual",
     "feature_count",
+    "feature_fertility_scores",
+    "fit_koopman_tensorprod",
     "fit_linear_operator",
+    "infer_scaling_domain",
+    "matrix_condition_number",
     "multi_indices",
+    "oak_score",
     "one_plus_lift",
     "polynomial_eval_from_lift",
+    "predict_lifted",
+    "residual_metrics",
+    "scale_to_unit_interval",
     "select_by_correlation_with_target",
     "select_by_variance",
+    "select_cvcd_features",
     "sparse_tensor_prod_lift",
     "tensor_prod_lift",
+    "train_test_koopman_oak",
 ]
