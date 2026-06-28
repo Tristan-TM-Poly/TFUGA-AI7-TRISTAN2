60a03ac1ac73c6bcb3f562b7fb44b6979601f18d
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-28 05:30:15 -0400

    Export Rosette adapter

--- omega_info2_tristan/src/omega_info2/__init__.py
+++ omega_info2_tristan/src/omega_info2/__init__.py
@@ -21,6 +21,7 @@ from .models import (
 )
 from .oak_gate import OAKInfoGate
 from .pipeline import Info2Pipeline, Info2PipelineResult
+from .rosette_adapter import RosetteClaim, RosetteExtraction, SourceSpan, rosette_to_info_object
 from .router import route_information
 from .source_trust import SourceTrustInput, score_source
 
@@ -47,10 +48,14 @@ __all__ = [
     "Provenance",
     "ProvenanceStep",
     "RawObject",
+    "RosetteClaim",
+    "RosetteExtraction",
     "Route",
+    "SourceSpan",
     "SourceTrustInput",
     "UncertaintyTensor",
     "Info2Graph",
+    "rosette_to_info_object",
     "score_source",
     "route_information",
     "freshness_score",
