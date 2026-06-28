defb9b0875eb1f34d7efa8d1c5293aa8c802a3e8
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-28 13:25:56 -0400

    feat: export dry-run connectors

--- packages/omega_action_ext_t/omega_action_ext_t/connectors/__init__.py
+++ packages/omega_action_ext_t/omega_action_ext_t/connectors/__init__.py
@@ -1,5 +1,16 @@
 """Dry-run connector interfaces for Ω-ACTION-EXT-T."""
 
-from .base import DryRunConnector, ConnectorPlan
+from .base import ConnectorPlan, DryRunConnector
+from .calendar_dryrun import CalendarDryRunConnector
+from .drive_dryrun import DriveDryRunConnector
+from .github_dryrun import GitHubDryRunConnector
+from .gmail_dryrun import GmailDryRunConnector
 
-__all__ = ["DryRunConnector", "ConnectorPlan"]
+__all__ = [
+    "CalendarDryRunConnector",
+    "ConnectorPlan",
+    "DriveDryRunConnector",
+    "DryRunConnector",
+    "GitHubDryRunConnector",
+    "GmailDryRunConnector",
+]
