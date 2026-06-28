d17fcff00acb2d939451eb9f36f49bb514ba0562
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-28 11:34:46 -0400

    feat: export plus ultra action modules

--- packages/omega_action_ext_t/omega_action_ext_t/__init__.py
+++ packages/omega_action_ext_t/omega_action_ext_t/__init__.py
@@ -1,5 +1,6 @@
 """Ω-ACTION-EXT-T: OAK-safe external action automation kernel."""
 
+from .approval_queue import ApprovalItem, ApprovalQueue
 from .core import (
     ActionDNA,
     AutonomyLevel,
@@ -8,14 +9,27 @@ from .core import (
     ProofOfExecution,
     RiskTensor,
 )
+from .incident_codex import IncidentRule, incident_rules
+from .ledger import LedgerRecord, ProofLedger
+from .manifest import ActionManifest
+from .oakbench import OAKBenchScore, score_report
 from .policy import OAKGate
 
 __all__ = [
     "ActionDNA",
+    "ActionManifest",
+    "ApprovalItem",
+    "ApprovalQueue",
     "AutonomyLevel",
     "Decision",
     "DryRunReport",
+    "IncidentRule",
+    "LedgerRecord",
+    "OAKBenchScore",
     "OAKGate",
+    "ProofLedger",
     "ProofOfExecution",
     "RiskTensor",
+    "incident_rules",
+    "score_report",
 ]
