99e9c1893045297bc29a12730300478ebbdac1e7
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-06-29 02:54:16 -0400

    Export PR build-to-green APIs

--- packages/omega_action_ext_t/omega_action_ext_t/__init__.py
+++ packages/omega_action_ext_t/omega_action_ext_t/__init__.py
@@ -11,6 +11,16 @@ from .core import (
     ProofOfExecution,
     RiskTensor,
 )
+from .green_builder import (
+    BuildAction,
+    GreenPlan,
+    GreenStep,
+    PRBlocker,
+    PRGreenState,
+    classify_blockers,
+    plan_build_to_green,
+    render_plan_markdown,
+)
 from .incident_codex import IncidentRule, incident_rules
 from .leak_scan import LeakFinding, has_findings, scan_text
 from .ledger import LedgerRecord, ProofLedger
@@ -18,6 +28,15 @@ from .manifest import ActionManifest
 from .oakbench import OAKBenchScore, score_report
 from .orchestrator import AutomationOrchestrator, AutomationResult
 from .policy import OAKGate
+from .pr_green_pipeline import (
+    PRGreenPacket,
+    action_for_plan,
+    build_green_packet,
+    build_green_packets,
+    render_batch_report,
+    risk_for_plan,
+    summarize_packets,
+)
 from .rollback import RollbackRecipe, recipe_for, recipes
 from .router import ConnectorRouter
 from .validators import validate_payload
@@ -34,24 +53,39 @@ __all__ = [
     "AutomationProfile",
     "AutomationResult",
     "AutonomyLevel",
+    "BuildAction",
     "ConnectorRouter",
     "Decision",
     "DryRunReport",
+    "GreenPlan",
+    "GreenStep",
     "IncidentRule",
     "LeakFinding",
     "LedgerRecord",
     "OAKBenchScore",
     "OAKGate",
+    "PRBlocker",
+    "PRGreenPacket",
+    "PRGreenState",
     "ProofLedger",
     "ProofOfExecution",
     "RiskTensor",
     "RollbackRecipe",
+    "action_for_plan",
+    "build_green_packet",
+    "build_green_packets",
+    "classify_blockers",
     "default_profiles",
     "has_findings",
     "incident_rules",
+    "plan_build_to_green",
     "recipe_for",
     "recipes",
+    "render_batch_report",
+    "render_plan_markdown",
+    "risk_for_plan",
     "scan_text",
     "score_report",
+    "summarize_packets",
     "validate_payload",
 ]
