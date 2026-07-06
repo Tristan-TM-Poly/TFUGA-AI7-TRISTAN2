b73e0edff81f24cdc5abac284ecfcc7813ffd518
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-07-06 13:15:50 -0400

    Export Gov QC v0.2 primitives

--- omega_gov_qc_t/src/omega_gov_qc_t/__init__.py
+++ omega_gov_qc_t/src/omega_gov_qc_t/__init__.py
@@ -1,13 +1,25 @@
 """Ω-GOV-QC-T / TristanGovGraph Québec prototype."""
 
+from .evidence import EvidenceGraph, EvidenceItem
 from .gov_graph import GovEdge, GovGraph, GovNode
 from .oak_gate import GateResult, OAKGate, OAKReport
+from .report_factory import GovReport, MarkdownReportFactory
+from .risk import RiskRegister, RiskTensor
+from .source_registry import SourceRecord, SourceRegistry
 
 __all__ = [
+    "EvidenceGraph",
+    "EvidenceItem",
     "GovEdge",
     "GovGraph",
     "GovNode",
     "GateResult",
+    "GovReport",
+    "MarkdownReportFactory",
     "OAKGate",
     "OAKReport",
+    "RiskRegister",
+    "RiskTensor",
+    "SourceRecord",
+    "SourceRegistry",
 ]
