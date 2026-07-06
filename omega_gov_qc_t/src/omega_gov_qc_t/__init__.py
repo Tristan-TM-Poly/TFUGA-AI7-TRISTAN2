002ef4021029a6f745f87543da1ff83e6323105b
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-07-06 14:26:07 -0400

    Export Gov QC ingestion and export primitives

--- omega_gov_qc_t/src/omega_gov_qc_t/__init__.py
+++ omega_gov_qc_t/src/omega_gov_qc_t/__init__.py
@@ -1,9 +1,12 @@
 """TristanGovGraph Quebec prototype."""
 
+from .dataset_health import DatasetHealthEngine, DatasetHealthReport, DatasetRecord
 from .evidence import EvidenceGraph, EvidenceItem
 from .gov_graph import GovEdge, GovGraph, GovNode
+from .json_exporter import ExportBundle, JsonExporter
 from .m_minus import MMinusEvent, MMinusRegister
 from .oak_gate import GateResult, OAKGate, OAKReport
+from .opendata_ingestor import IngestionResult, OpenDataIngestor
 from .product_factory import ProductCard, ProductFactory
 from .report_factory import GovReport, MarkdownReportFactory
 from .risk import RiskRegister, RiskTensor
@@ -11,18 +14,25 @@ from .service_model import PublicService, ServiceCatalog
 from .source_registry import SourceRecord, SourceRegistry
 
 __all__ = [
+    "DatasetHealthEngine",
+    "DatasetHealthReport",
+    "DatasetRecord",
     "EvidenceGraph",
     "EvidenceItem",
+    "ExportBundle",
     "GateResult",
     "GovEdge",
     "GovGraph",
     "GovNode",
     "GovReport",
+    "IngestionResult",
+    "JsonExporter",
     "MMinusEvent",
     "MMinusRegister",
     "MarkdownReportFactory",
     "OAKGate",
     "OAKReport",
+    "OpenDataIngestor",
     "ProductCard",
     "ProductFactory",
     "PublicService",
