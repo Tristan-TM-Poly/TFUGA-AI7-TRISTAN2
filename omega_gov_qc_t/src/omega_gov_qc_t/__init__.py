d7e92f92a2731a369515992f9c812c97da64c4f7
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-07-06 12:59:55 -0400

    Add Omega Gov QC Python package exports

---
+++ omega_gov_qc_t/src/omega_gov_qc_t/__init__.py
@@ -0,0 +1,13 @@
+"""Ω-GOV-QC-T / TristanGovGraph Québec prototype."""
+
+from .gov_graph import GovEdge, GovGraph, GovNode
+from .oak_gate import GateResult, OAKGate, OAKReport
+
+__all__ = [
+    "GovEdge",
+    "GovGraph",
+    "GovNode",
+    "GateResult",
+    "OAKGate",
+    "OAKReport",
+]
