fd9b93c168d88dd1a760463723b69e6829be6058
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-07-06 13:50:21 -0400

    Export Gov QC v0.3 models

--- omega_gov_qc_t/src/omega_gov_qc_t/__init__.py
+++ omega_gov_qc_t/src/omega_gov_qc_t/__init__.py
@@ -1,25 +1,34 @@
-"""Ω-GOV-QC-T / TristanGovGraph Québec prototype."""
+"""TristanGovGraph Quebec prototype."""
 
 from .evidence import EvidenceGraph, EvidenceItem
 from .gov_graph import GovEdge, GovGraph, GovNode
+from .m_minus import MMinusEvent, MMinusRegister
 from .oak_gate import GateResult, OAKGate, OAKReport
+from .product_factory import ProductCard, ProductFactory
 from .report_factory import GovReport, MarkdownReportFactory
 from .risk import RiskRegister, RiskTensor
+from .service_model import PublicService, ServiceCatalog
 from .source_registry import SourceRecord, SourceRegistry
 
 __all__ = [
     "EvidenceGraph",
     "EvidenceItem",
+    "GateResult",
     "GovEdge",
     "GovGraph",
     "GovNode",
-    "GateResult",
     "GovReport",
+    "MMinusEvent",
+    "MMinusRegister",
     "MarkdownReportFactory",
     "OAKGate",
     "OAKReport",
+    "ProductCard",
+    "ProductFactory",
+    "PublicService",
     "RiskRegister",
     "RiskTensor",
+    "ServiceCatalog",
     "SourceRecord",
     "SourceRegistry",
 ]
