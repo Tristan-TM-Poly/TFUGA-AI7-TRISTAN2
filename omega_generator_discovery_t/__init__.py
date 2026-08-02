eebde6bfd29e523c0e63cc1f9521d6c0921650d9
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-08-02 14:27:40 -0400

    feat(generator-r03): expose campaign API

--- omega_generator_discovery_t/__init__.py
+++ omega_generator_discovery_t/__init__.py
@@ -1,5 +1,19 @@
 """Ω-GENERATOR-DISCOVERY-STACK public API."""
 from .autolab import ExperimentCandidate, ExperimentDecision, prioritize_experiments
+from .campaign import (
+    CampaignAxes,
+    CampaignEmitter,
+    CampaignEmissionReport,
+    CampaignPartition,
+    CampaignSpec,
+    benchmark_addition,
+    generator_addition,
+    iter_generator_bundles,
+    load_campaign_spec,
+    mixed_radix_decode,
+    partition_campaign,
+    stream_digest,
+)
 from .core import (
     AffineGenerator1D,
     GeneratorSyndrome,
@@ -20,16 +34,19 @@ from .protocol import InstrumentProtocol, compile_protocol
 from .spectral import SpectralMorph, compare_spectra, lorentzian, mixture
 
 __all__ = [
-    "AffineGenerator1D", "EpistemicTransition", "ExperimentCandidate",
-    "ExperimentDecision", "GeneratorSyndrome", "HolonomyReport",
-    "InstrumentProtocol", "LinearGeneratorOperator", "MorphIR",
-    "FRONTS", "FrontSpec", "OrderExperiment", "SpectralMorph",
+    "AffineGenerator1D", "CampaignAxes", "CampaignEmitter",
+    "CampaignEmissionReport", "CampaignPartition", "CampaignSpec",
+    "EpistemicTransition", "ExperimentCandidate", "ExperimentDecision",
+    "GeneratorSyndrome", "HolonomyReport", "InstrumentProtocol",
+    "LinearGeneratorOperator", "MorphIR", "FRONTS", "FrontSpec",
+    "OrderExperiment", "SpectralMorph", "benchmark_addition",
     "compare_spectra", "compile_morph_ir", "compile_protocol",
     "crystal_holonomy", "design_order_experiment",
-    "evidence_growth_transition", "fit_scalar_generator",
-    "front_registry", "generator_syndrome", "identify_affine_1d",
-    "lorentzian", "mixture", "prioritize_experiments",
-    "semigroup_defect",
+    "evidence_growth_transition", "fit_scalar_generator", "front_registry",
+    "generator_addition", "generator_syndrome", "identify_affine_1d",
+    "iter_generator_bundles", "load_campaign_spec", "lorentzian",
+    "mixed_radix_decode", "mixture", "partition_campaign",
+    "prioritize_experiments", "semigroup_defect", "stream_digest",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
