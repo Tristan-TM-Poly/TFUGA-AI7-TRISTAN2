5e1d85111a0b7f17f6987a66f2d84dd239c111a7
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-08-02 14:27:09 -0400

    feat(omega-unbounded): export R0.5 governance primitives

--- omega_unbounded_t/__init__.py
+++ omega_unbounded_t/__init__.py
@@ -24,6 +24,17 @@ from .github_planner import (
     iter_jsonl,
     synthetic_additions,
 )
+from .governance import (
+    IterationObservation,
+    ObjectiveVector,
+    ReflexMemoryLedger,
+    ReflexRule,
+    StopDecision,
+    StopGate,
+    StopPolicy,
+    dominates,
+    pareto_front,
+)
 from .self_improvement import (
     ControllerVariant,
     PromotionDecision,
@@ -58,12 +69,16 @@ __all__ = [
     "GitHubDryRunPlanner",
     "GitHubPlanPolicy",
     "GitHubPlanReport",
+    "IterationObservation",
     "ListWorkSource",
     "MMinusLedger",
     "MPlusLedger",
+    "ObjectiveVector",
     "PromotionDecision",
     "RangeWorkSource",
     "RawSelfImprovementLab",
+    "ReflexMemoryLedger",
+    "ReflexRule",
     "ResourceAwareSelfImprovementLab",
     "ResourceSampler",
     "ResourceSnapshot",
@@ -73,13 +88,18 @@ __all__ = [
     "SelfImprovementReport",
     "SelfImprovementScenario",
     "ShardRecord",
+    "StopDecision",
+    "StopGate",
+    "StopPolicy",
     "SyntheticCapacityExecutor",
     "VariantOutcome",
     "adaptive_candidate_stream",
     "default_scenarios",
+    "dominates",
     "iter_jsonl",
     "iter_variants_jsonl",
+    "pareto_front",
     "synthetic_additions",
 ]
 
-__version__ = "0.4.1"
+__version__ = "0.5.0"
