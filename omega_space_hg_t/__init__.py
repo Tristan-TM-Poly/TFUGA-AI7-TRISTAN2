6597994aedca22fc669846a876008e084d0fe9aa
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-08-03 14:34:46 -0400

    feat(space): expose generative space-system atlas

--- omega_space_hg_t/__init__.py
+++ omega_space_hg_t/__init__.py
@@ -1,5 +1,15 @@
 """Ω-SPACE-HG-T∞: OAK-safe hypergraph spacecraft research kernel."""
 
+from .atlas import (
+    ENVIRONMENT_CLASSES,
+    FIDELITY_LEVELS,
+    MISSION_CLASSES,
+    SIMULATION_DOMAINS,
+    SUBSYSTEM_CLASSES,
+    VEHICLE_AND_MODULE_CLASSES,
+    atlas_manifest,
+    select_atlas_slice,
+)
 from .hypergraph import SpaceHyperedge, SpaceHypergraph, SpaceNode, build_spacecraft_hypergraph
 from .io import emit_json, load_mission, mission_from_dict
 from .mission import DEFAULT_SUBSYSTEMS, compile_mission_hypergraph, simulate_mission
@@ -37,16 +47,23 @@ __all__ = [
     "DesignEvaluation",
     "EARTH_MU_M3_S2",
     "EARTH_RADIUS_M",
+    "ENVIRONMENT_CLASSES",
+    "FIDELITY_LEVELS",
+    "MISSION_CLASSES",
     "MissionConfig",
     "MissionMetrics",
     "MissionResult",
     "OrbitState",
+    "SIMULATION_DOMAINS",
+    "SUBSYSTEM_CLASSES",
     "SimulationPoint",
     "SpaceHyperedge",
     "SpaceHypergraph",
     "SpaceNode",
     "SpacecraftConfig",
     "UnboundedDesignFrontier",
+    "VEHICLE_AND_MODULE_CLASSES",
+    "atlas_manifest",
     "build_spacecraft_hypergraph",
     "canonical_6u_mission",
     "circular_orbit_state",
@@ -61,6 +78,7 @@ __all__ = [
     "propagate_two_body",
     "relative_energy_drift",
     "run_oak_benchmarks",
+    "select_atlas_slice",
     "semimajor_axis_m",
     "simulate_mission",
     "specific_angular_momentum",
