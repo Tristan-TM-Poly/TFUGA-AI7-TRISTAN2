7a6a39f49d628de271b1a237a7b573a08033835f
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-08-03 10:25:33 -0400

    feat(omega-vla): expose differential and decomposition API

--- omega_vla_t/__init__.py
+++ omega_vla_t/__init__.py
@@ -6,6 +6,28 @@ until formalized, benchmarked, and independently validated.
 """
 
 from .core import LinearOperator, SVDReport, VectorSpace
+from .decompositions import (
+    ProjectorAudit,
+    SymmetryDecomposition,
+    anticommutator,
+    audit_orthogonal_projector,
+    commutator,
+    decompose_symmetric_skew,
+    metric_projector,
+    orthogonal_projector,
+    orthonormal_basis,
+    principal_angles,
+    projection_residual,
+)
+from .differential import (
+    LinearizationReport,
+    audit_linearization,
+    directional_derivative,
+    gradient_fd,
+    hessian,
+    jacobian,
+    propagate_covariance,
+)
 from .oak import OAKCheck, OAKReport, audit_operator, basis_covariance_error
 from .vector_calculus import (
     GraphHodgeReport,
@@ -23,20 +45,38 @@ from .vector_calculus import (
 __all__ = [
     "GraphHodgeReport",
     "LinearOperator",
+    "LinearizationReport",
     "OAKCheck",
     "OAKReport",
+    "ProjectorAudit",
     "SVDReport",
+    "SymmetryDecomposition",
     "VectorSpace",
+    "anticommutator",
+    "audit_linearization",
     "audit_operator",
+    "audit_orthogonal_projector",
     "basis_covariance_error",
+    "commutator",
     "curl_2d",
+    "decompose_symmetric_skew",
+    "directional_derivative",
     "divergence",
     "gradient",
+    "gradient_fd",
     "graph_divergence",
     "graph_gradient",
     "graph_hodge_decomposition",
     "graph_laplacian",
+    "hessian",
+    "jacobian",
     "laplacian",
+    "metric_projector",
+    "orthogonal_projector",
+    "orthonormal_basis",
+    "principal_angles",
+    "projection_residual",
+    "propagate_covariance",
     "validate_incidence",
 ]
 
