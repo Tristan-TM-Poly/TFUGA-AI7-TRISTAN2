a34f8bb1f315732780fab25c5b007ac7e4db7249
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-08-07 11:45:54 -0400

    build(summary): expose R0.3 longitudinal APIs

--- omega_summary_fractal_t/__init__.py
+++ omega_summary_fractal_t/__init__.py
@@ -1,6 +1,14 @@
 """Ω-SUMMARY-FRACTAL-T∞: deterministic multi-depth repository summarization."""
 
 from .delta import delta_summaries, render_delta_markdown, write_delta
+from .export import write_graph_exports, write_graphml, write_jsonl
+from .index import (
+    append_snapshot,
+    longitudinal_metrics,
+    normalize_snapshot,
+    verify_index,
+    write_longitudinal_reports,
+)
 from .lineage import build_system_lineage, convergence_candidates, proof_debt
 from .models import EvidenceRef, SummaryBundle, SummaryNode
 from .summarizer import SummaryEngine, build_summary
@@ -17,6 +25,14 @@ __all__ = [
     "delta_summaries",
     "render_delta_markdown",
     "write_delta",
+    "append_snapshot",
+    "longitudinal_metrics",
+    "normalize_snapshot",
+    "verify_index",
+    "write_longitudinal_reports",
+    "write_graph_exports",
+    "write_graphml",
+    "write_jsonl",
 ]
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
