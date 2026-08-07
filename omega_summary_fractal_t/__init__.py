a631136679e62e374caf24b3381782b60453b086
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-08-07 11:34:02 -0400

    build(summary): expose R0.2 lineage and delta API

--- omega_summary_fractal_t/__init__.py
+++ omega_summary_fractal_t/__init__.py
@@ -1,5 +1,7 @@
 """Ω-SUMMARY-FRACTAL-T∞: deterministic multi-depth repository summarization."""
 
+from .delta import delta_summaries, render_delta_markdown, write_delta
+from .lineage import build_system_lineage, convergence_candidates, proof_debt
 from .models import EvidenceRef, SummaryBundle, SummaryNode
 from .summarizer import SummaryEngine, build_summary
 
@@ -9,6 +11,12 @@ __all__ = [
     "SummaryNode",
     "SummaryEngine",
     "build_summary",
+    "build_system_lineage",
+    "convergence_candidates",
+    "proof_debt",
+    "delta_summaries",
+    "render_delta_markdown",
+    "write_delta",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
