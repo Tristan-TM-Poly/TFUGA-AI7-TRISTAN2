71dab32f1415965db2da20724be5b22af0450999
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-08-09 15:30:19 -0400

    feat(actions): expose evidence bundle API

--- omega_actions_t/__init__.py
+++ omega_actions_t/__init__.py
@@ -2,6 +2,7 @@
 
 from .analyzer import analyze_repository, analyze_workflow, render_markdown, write_report
 from .delta_ci import plan_delta, write_delta_report
+from .evidence import build_evidence_bundle, write_bundle
 from .telemetry import analyze_telemetry, write_telemetry_report
 
 __all__ = [
@@ -13,4 +14,6 @@ __all__ = [
     "write_delta_report",
     "analyze_telemetry",
     "write_telemetry_report",
+    "build_evidence_bundle",
+    "write_bundle",
 ]
