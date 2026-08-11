6eff2dc7dffd1543d37e2e65999615fe369869bf
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-08-11 13:58:25 -0400

    feat(zeta-square): export R2 certificate and reconstruction APIs

--- omega_zeta_square_t/__init__.py
+++ omega_zeta_square_t/__init__.py
@@ -3,6 +3,16 @@
 This package is a research/verification toolkit. It does not claim a proof of RH.
 """
 
+from .certificates import (
+    ExactPSDReport,
+    ExactStieltjesCertificate,
+    exact_determinant,
+    exact_hankel_matrix,
+    exact_principal_minors,
+    exact_psd_report,
+    exact_stieltjes_certificate,
+    leading_only_false_positive_hankel,
+)
 from .core import (
     SquareCoordinate,
     centered_square,
@@ -14,13 +24,25 @@ from .core import (
     strip_boundary,
     trivial_zero_image,
 )
+from .jacobi import (
+    JacobiRecurrence,
+    jacobi_characteristic_polynomial,
+    jacobi_recurrence_from_inverse_moments,
+)
 from .moments import (
     finite_stieltjes_report,
     hankel_matrix,
     inverse_even_moments,
+    ldlt_psd,
     leading_principal_minors,
 )
 from .oak import ClaimStatus, OakClaim, validate_claim
+from .pade import (
+    PadeApproximant,
+    pade_from_series,
+    stieltjes_pade_from_inverse_moments,
+    stieltjes_series_from_inverse_moments,
+)
 from .proof_graph import load_and_validate_proof_graph, validate_proof_graph
 from .series import (
     inverse_moments_from_theta_coeffs,
@@ -41,8 +63,24 @@ __all__ = [
     "trivial_zero_image",
     "inverse_even_moments",
     "hankel_matrix",
+    "ldlt_psd",
     "leading_principal_minors",
     "finite_stieltjes_report",
+    "ExactPSDReport",
+    "ExactStieltjesCertificate",
+    "exact_determinant",
+    "exact_hankel_matrix",
+    "exact_principal_minors",
+    "exact_psd_report",
+    "exact_stieltjes_certificate",
+    "leading_only_false_positive_hankel",
+    "PadeApproximant",
+    "pade_from_series",
+    "stieltjes_series_from_inverse_moments",
+    "stieltjes_pade_from_inverse_moments",
+    "JacobiRecurrence",
+    "jacobi_recurrence_from_inverse_moments",
+    "jacobi_characteristic_polynomial",
     "ClaimStatus",
     "OakClaim",
     "validate_claim",
