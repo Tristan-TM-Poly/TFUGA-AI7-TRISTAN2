995a16b833ad76ea036989371aabf087ec047ca1
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-08-12 16:42:31 -0400

    feat(compute): expose R0.2 validation and diff API

--- omega_compute_physics_t/__init__.py
+++ omega_compute_physics_t/__init__.py
@@ -7,7 +7,16 @@ mathematical proof of asymptotic Big-O/Theta complexity.
 """
 
 from .atlas import ComplexityAtlas, EmpiricalResourceModel, ResourceSample
+from .complexity_diff import ComplexityDiffReport, compare_models, geometric_sweep
 from .profiler import ProfileResult, profile_call, profile_pipeline
+from .validation import (
+    ConformalInterval,
+    DriftReport,
+    ModelCandidate,
+    ValidatedResourceModel,
+    detect_drift,
+    fit_validated_resource_model,
+)
 
 __all__ = [
     "ComplexityAtlas",
@@ -16,6 +25,15 @@ __all__ = [
     "ProfileResult",
     "profile_call",
     "profile_pipeline",
+    "ModelCandidate",
+    "ValidatedResourceModel",
+    "ConformalInterval",
+    "DriftReport",
+    "fit_validated_resource_model",
+    "detect_drift",
+    "ComplexityDiffReport",
+    "compare_models",
+    "geometric_sweep",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
