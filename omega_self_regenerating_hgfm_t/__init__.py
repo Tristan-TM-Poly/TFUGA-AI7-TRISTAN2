bac7517d0aaadb012991da8fbdf983b5d49468cd
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-08-13 22:36:54 -0400

    feat: add self-regenerating HGFM package init

---
+++ omega_self_regenerating_hgfm_t/__init__.py
@@ -0,0 +1,10 @@
+from .core import (
+    EpistemicNode, HyperedgeContract, ResidualField,
+    MinusMemory, PlusMemory, MorphogenesisPolicy,
+    RegenerationBench, SelfRegeneratingHGFM,
+)
+__all__ = [
+    "EpistemicNode","HyperedgeContract","ResidualField",
+    "MinusMemory","PlusMemory","MorphogenesisPolicy",
+    "RegenerationBench","SelfRegeneratingHGFM",
+]
