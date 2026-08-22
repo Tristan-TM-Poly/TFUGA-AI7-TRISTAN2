84c30f7705eb8947fa775a06ab4ccae597402661
Tristan-TM-Poly <tristan.tardif-morency@polymtl.ca>
2026-08-22 02:49:34 -0400

    feat(metatime): expose public API

---
+++ omega_metatime/__init__.py
@@ -0,0 +1,21 @@
+from .core import (
+    CapabilityDelta,
+    MetaTimeEngine,
+    StrategyGenome,
+    StudentBaselineTwin,
+    TemporalCounters,
+    TemporalCrystal,
+    TemporalRegime,
+    TemporalState,
+)
+
+__all__ = [
+    "CapabilityDelta",
+    "MetaTimeEngine",
+    "StrategyGenome",
+    "StudentBaselineTwin",
+    "TemporalCounters",
+    "TemporalCrystal",
+    "TemporalRegime",
+    "TemporalState",
+]
